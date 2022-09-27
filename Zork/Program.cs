649bb7dbd0899a3ff2f6ea99bb9ff227ac1fd4f7
SyuenTong <a.syuen97@gmail.com>
2022-09-26 20:13:32 -0400

    Zork 4.1 Descriptions not printing

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,74 +1,157 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zork
 {
     class Program
     {
+        private static Room CurrentRoom
+        {
+            get
+            {
+                return _rooms[_location.Vertical, _location.Horizontal];
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            const string defaultRoomsFilename = "Rooms.txt";
+            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
+            InitalizeRoomDescriptions(roomsFilename);
 
-            static Commands ToCommand(string commandString)
+            Room previousRoom = null;
+            bool isRunning = true;
+            while (isRunning)
             {
-                if (Enum.TryParse<Commands>(commandString, true, out Commands command))
-                {
-                    return command;
-                }
-                else
-                {
-                    return Commands.Unknown;
-                }
-            }
+                Console.WriteLine($"{CurrentRoom}");
 
-            private static bool Move(Commands command)
-            {
-                bool didMove = false;
+                if (previousRoom != CurrentRoom && CurrentRoom.Visited == false)
+                {
+                    Console.WriteLine(CurrentRoom.Description);
+                    previousRoom = CurrentRoom;
+                    CurrentRoom.Visited = true;
+                }
+
+                Console.Write("> ");
+
+                string inputString = Console.ReadLine().Trim();
+                Commands command = ToCommand(inputString);
+
+                string outputString;
                 switch (command)
                 {
-                    case Commands.North when _location.Vertical < _rooms.GetLength(1) - 1:
-                        _location.Vertical++;
-                        didMove = true;
+                    case Commands.Quit:
+                        isRunning = false;
+                        outputString = "Thank you for playing!";
                         break;
-                    case Commands.South when _location.Vertical > 0:
-                        _location.Vertical--;
-                        didMove = true;
+                    case Commands.Look:
+                        outputString = (CurrentRoom.Description);
+                        break;
+                    case Commands.North:
+                    case Commands.South:
+                    case Commands.East:
+                    case Commands.West:
+                        if (Move(command))
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
                         break;
 
-                    case Commands.East when _location.Horizontal < _rooms.GetLength(0) - 1:
-                        _location.Horizontal++;
-                        didMove = true;
-                        break;
-                    case Commands.West when _location.Horizontal > 0:
-                        _location.Horizontal--;
-                        didMove = true;
-                        break;
                     default:
+                        outputString = "Unknown command.";
                         break;
                 }
-                return didMove;
-            }
+                Console.WriteLine(outputString);
 
-            private static void InitalizeRoomDescriptions()
+            }
+        }
+
+        static Commands ToCommand(string commandString)
+        {
+            if (Enum.TryParse<Commands>(commandString, true, out Commands command))
             {
-                var roomMap = new Dictionary<string, Room>();
-                foreach (Room room in _rooms)
+                return command;
+            }
+            else
+            {
+                return Commands.Unknown;
+            }
+        }
+
+        static Program()
+        {
+            RoomMap = new Dictionary<string, Room>();
+            foreach (Room room in _rooms)
+            {
+                RoomMap[room.Name] = room;
+            }
+        }
+
+        private static bool Move(Commands command)
+        {
+            bool didMove = false;
+            switch (command)
+            {
+                case Commands.North when _location.Vertical < _rooms.GetLength(1) - 1:
+                    _location.Vertical++;
+                    didMove = true;
+                    break;
+                case Commands.South when _location.Vertical > 0:
+                    _location.Vertical--;
+                    didMove = true;
+                    break;
+
+                case Commands.East when _location.Horizontal < _rooms.GetLength(0) - 1:
+                    _location.Horizontal++;
+                    didMove = true;
+                    break;
+                case Commands.West when _location.Horizontal > 0:
+                    _location.Horizontal--;
+                    didMove = true;
+                    break;
+                default:
+                    break;
+            }
+            return didMove;
+        }
+
+        private static void InitalizeRoomDescriptions(string roomsFilename)
+        {
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+
+            string[] lines = File.ReadAllLines(roomsFilename);
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(fieldDelimiter);
+                if (fields.Length != expectedFieldCount)
                 {
-                    roomMap.Add(room.Name, room);
+                    throw new InvalidDataException("Invalid record.");
                 }
 
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon ";
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
 
-                roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-                roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-                roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window wich is slightly ajar.";
-
-                roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-                roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-                roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+                RoomMap[name].Description = description;
             }
+        }
+
+        private enum Fields
+        {
+            Name = 0,
+            Description
+        }
+
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
 
         private static readonly Room[,] _rooms =
         {
@@ -78,6 +161,7 @@ namespace Zork
         };
 
         private static (int Vertical, int Horizontal) _location = (1, 1);
+        private static readonly Dictionary<string, Room> RoomMap;
+
     }
 }
-
