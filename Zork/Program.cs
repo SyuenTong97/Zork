b707a6f75f3d9824d40e4e03adbfc91b76b1e202
SyuenTong <a.syuen97@gmail.com>
2022-10-03 21:50:59 -0400

    Zork 4.2 Json file syntax error

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace Zork
 {
@@ -10,16 +11,16 @@ namespace Zork
         {
             get
             {
-                return _rooms[_location.Vertical, _location.Horizontal];
+                return Rooms[_location.Vertical, _location.Horizontal];
             }
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            const string defaultRoomsFilename = "Rooms.txt";
+            const string defaultRoomsFilename = "Rooms.json";
             string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
-            InitalizeRoomDescriptions(roomsFilename);
+            InitalizeRooms(roomsFilename);
 
             Room previousRoom = null;
             bool isRunning = true;
@@ -82,21 +83,12 @@ namespace Zork
             }
         }
 
-        static Program()
-        {
-            RoomMap = new Dictionary<string, Room>();
-            foreach (Room room in _rooms)
-            {
-                RoomMap[room.Name] = room;
-            }
-        }
-
         private static bool Move(Commands command)
         {
             bool didMove = false;
             switch (command)
             {
-                case Commands.North when _location.Vertical < _rooms.GetLength(1) - 1:
+                case Commands.North when _location.Vertical < Rooms.GetLength(1) - 1:
                     _location.Vertical++;
                     didMove = true;
                     break;
@@ -105,7 +97,7 @@ namespace Zork
                     didMove = true;
                     break;
 
-                case Commands.East when _location.Horizontal < _rooms.GetLength(0) - 1:
+                case Commands.East when _location.Horizontal < Rooms.GetLength(0) - 1:
                     _location.Horizontal++;
                     didMove = true;
                     break;
@@ -119,26 +111,8 @@ namespace Zork
             return didMove;
         }
 
-        private static void InitalizeRoomDescriptions(string roomsFilename)
-        {
-            const string fieldDelimiter = "##";
-            const int expectedFieldCount = 2;
-
-            string[] lines = File.ReadAllLines(roomsFilename);
-            foreach (string line in lines)
-            {
-                string[] fields = line.Split(fieldDelimiter);
-                if (fields.Length != expectedFieldCount)
-                {
-                    throw new InvalidDataException("Invalid record.");
-                }
-
-                string name = fields[(int)Fields.Name];
-                string description = fields[(int)Fields.Description];
-
-                RoomMap[name].Description = description;
-            }
-        }
+        private static void InitalizeRooms(string roomsFilename) =>
+            Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
 
         private enum Fields
         {
@@ -151,15 +125,7 @@ namespace Zork
             RoomsFilename = 0
         }
 
-        private static readonly Room[,] _rooms =
-        {
-            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
-            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
-        };
-
+        private static Room[,] Rooms;
         private static (int Vertical, int Horizontal) _location = (1, 1);
-        private static readonly Dictionary<string, Room> RoomMap;
-
     }
 }
